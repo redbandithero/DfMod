9c6645534747137e525d7aa36ced7c195acf272a
redbandithero <rbandithero@gmail.com>
2023-05-20 15:45:05 +1000

    Initial Commit

---
+++ src/main/java/io/github/redbandithero/dfmod/DfMod.java
@@ -0,0 +1,14 @@
+package io.github.redbandithero.dfmod;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.tinyremapper.extension.mixin.common.Logger;
+
+public class DfMod implements ModInitializer {
+    /**
+     * Runs the mod initializer.
+     */
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/io/github/redbandithero/dfmod/client/DfModClient.java
@@ -0,0 +1,13 @@
+package io.github.redbandithero.dfmod.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DfModClient implements ClientModInitializer {
+    /**
+     * Runs the mod initializer on the client environment.
+     */
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
